3e1f48837cc610be9c6a729bd2fedcb87caeb82e
Tarun <treddy93@gmail.com>
2018-10-02 02:12:16 +0000

    commimtting all files

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,25 @@
-var request =  require('request');
+var request = require('request');
+// var https = require('https')
 
-console.log('Welcome GitHub Avatar Downloader!');
+// console.log('Welcome GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+}
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+
+
