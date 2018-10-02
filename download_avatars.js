87f073bffc2bf7305bb2fe5cf378b410e2e5eb28
Tarun <treddy93@gmail.com>
2018-10-02 03:43:04 +0000

    completed the first part of the exercise

--- download_avatars.js
+++ download_avatars.js
@@ -1,25 +1,34 @@
 var request = require('request');
+var secrets = require('./secrets')
 // var https = require('https')
 
 // console.log('Welcome GitHub Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, cb) {
+function getRepoContributors(repoOwner, repoName, handleFetchAvatarURL) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
+      'Authorization': secrets.Authorizations
     }
   };
 
   request(options, function(err, res, body) {
-    cb(err, body);
+
+    var contributors = JSON.parse(body)
+    for(var i = 0; i < contributors.length; i++){
+      var contributor = contributors[i];
+      handleFetchAvatarURL(contributor.avatar_url);
+    };
   });
+};
+
+var fetchAvatarURL = function(avatarURL){
+  console.log(avatarURL);
 }
+  // console.log("Result:", result);
 
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-});
+getRepoContributors("jquery", "jquery", fetchAvatarURL);
 
 
